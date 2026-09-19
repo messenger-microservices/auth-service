fcfdbaf4f5978a7a99036f40c7ba68fc82f0028c
pulsar <pulsarmn@yandex.ru>
2026-09-19 13:57:36 +0300

    chore: add Liquibase migration for AuthUser entity

---
+++ src/main/resources/db/changelog/0.1.0/changesets/2026_09_19_auth_user_entity.sql
@@ -0,0 +1,16 @@
+--liquibase formatted sql
+
+--changeset pulsarmn:create_auth_users_table
+CREATE TABLE auth_users
+(
+    id            UUID PRIMARY KEY,
+    username      VARCHAR(32)  NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    phone_number  VARCHAR(50)  NULL,
+    create_at     TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
+    updated_at    TIMESTAMPTZ  NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX idx_auth_users_username ON auth_users (username);
+
+--rollback DROP TABLE auth_users;
