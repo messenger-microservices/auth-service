9677871d3e5efab7b410a7eb76cb0d7c02929eab
pulsar <pulsarmn@yandex.ru>
2026-09-19 12:18:42 +0300

    chore: add necessary dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,16 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    implementation(platform(libs.spring.boot.dependencies))
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.spring.boot.starter.liquibase)
+}
+
+dependencies {
+    runtimeOnly(libs.postgresql.driver)
+}
+
 configure<JavaPluginExtension> {
     toolchain {
         languageVersion = JavaLanguageVersion.of(25)
